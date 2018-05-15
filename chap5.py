18680f1013f7a5b3cf40796a12a7c631d6c8d5b1
Kyle Shive <shivekyle@gmail.com>
2018-05-15 13:05:38 -0500

    more practice in chap5 CC

---
+++ chap5.py
@@ -0,0 +1,35 @@
+'''
+game = 'FF8'
+
+print("Is game == 'FF8'? I predict True.")
+print(game == 'FF8')
+
+print("\nIs game == 'FF9'? I predict False.")
+print(game == 'FF9')
+print("\n")
+print('hello' == 'Hello')
+print('hello' == 'hello')
+
+x = 'MONKEY'
+
+print(x.lower() == 'monkey')
+print (x)
+
+print(5 < 2 or 4 < 10)
+
+test = [print(value) for value in list(range(1, 21) )]
+'''
+
+temp_list = list(range(1, 21))
+
+if 10 in temp_list:
+    print("the number 10 is in the list")
+
+
+print(30 not in temp_list)
+
+if 30 not in temp_list:
+    print("The value 30 is not in the list")
+
+
+
