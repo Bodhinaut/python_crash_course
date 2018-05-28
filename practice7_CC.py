19f574a4082f7b668260983534d1f144fab361fa
Kyle Shive <shivekyle@gmail.com>
2018-05-28 15:15:40 -0500

    practice 7, user input and while loops

---
+++ practice7_CC.py
@@ -0,0 +1,38 @@
+#message = (input("Tell me something, and I will repeat it back to you: ") )
+#print(message)
+
+#print("-----------------------------------------------------------------------")
+
+#prompt = "If you tell us who you are, we can personalize the messages you see."
+#prompt += "\nWhat is your first name? "
+
+#name = input(prompt)
+#print("\nHello " + str(name.title() ) + "." )
+
+#age = input("How old are you? ")
+#age = int(age)
+#print(age)
+
+
+
+#rental_car = input("What kind of rental car would you like to see? : ")
+#print("Let me see if I can find you a " + rental_car.title() + "!")
+
+'''
+ppl_table = input("How many people are in your dinner group? : ")
+ppl_table = int(ppl_table)
+
+if ppl_table > 8:
+	print("I am sorry, for " + str(ppl_table) + " people you will have to wait.")
+else:
+	print("Great, right this way!")
+
+'''
+number = input("Please, give me the Great Binary Buddha a number: ")
+number = int(number)
+
+if number % 10 == 0:
+	print(str(number) + ", is a multiple of 10.")
+else:
+	print(str(number) + " is not a multiple of 10.")
+
