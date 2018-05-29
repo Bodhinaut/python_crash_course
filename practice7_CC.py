bfa3468a452d70cedf819cba345de65649a2bf0e
Kyle Shive <shivekyle@gmail.com>
2018-05-29 11:04:47 -0500

    initial

--- practice7_CC.py
+++ practice7_CC.py
@@ -123,6 +123,7 @@ while True:
 		print("Damn you old, just go in...")
 		break
 '''
+'''
 
 unconfirmed_users = ['alice', 'brian', 'candace']
 confirmed_users = []
@@ -137,6 +138,34 @@ while unconfirmed_users:
 	print("\nThe following users have been confirmed: ")
 	for confirmed_user in confirmed_users:
 		print(confirmed_user.title() )
+
+'''
+
+name_mtn_quest = {}
+
+# Set a flag to indicate that polling is active
+
+polling_active = True
+
+while polling_active:
+	#Prompt for the person's name and response.
+	name = input("\nWhat is your name? < ")
+	mtn_quest = input("Which mountain would you like to climb someday? < ")
+
+	#Store response in the dictionary:
+	name_mtn_quest[name] = mtn_quest
+
+	#Anyone else want to take?
+	repeat = input("Would you like to let another person respond? (yes/no) < ")
+	if repeat.lower() == 'no':
+		polling_active = False
+
+	#Polling complete, show results,
+
+	print("\n--- Poll Results ---")
+	for name, mtn_quest in name_mtn_quest.items():
+		print(name.title() + " would like to climb " + mtn_quest.title() + ".")
+
 
 
 
