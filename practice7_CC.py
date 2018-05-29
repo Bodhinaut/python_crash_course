559ede65edb4724fd67ac2a94d989d7c2876bbdf
Kyle Shive <shivekyle@gmail.com>
2018-05-29 10:39:22 -0500

    looping dictionaries with while loops;

--- practice7_CC.py
+++ practice7_CC.py
@@ -100,6 +100,8 @@ while True:
 	
 '''
 
+'''
+
 while True:
 	try:
 		message = int(input("What is your age? : ") )
@@ -120,3 +122,21 @@ while True:
 	elif message > 99:
 		print("Damn you old, just go in...")
 		break
+'''
+
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+while unconfirmed_users:
+	current_user = unconfirmed_users.pop()
+
+	print("Verifying user: " + current_user.title() )
+	confirmed_users.append(current_user)
+
+	# Display all confirmed users.
+	print("\nThe following users have been confirmed: ")
+	for confirmed_user in confirmed_users:
+		print(confirmed_user.title() )
+
+
+
