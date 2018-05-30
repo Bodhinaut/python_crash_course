17ca24e2f8504c829b3556ae9b98b866d6b5fdbe
Kyle Shive <shivekyle@gmail.com>
2018-05-30 14:42:25 -0500

    challenges complete

--- try_it_yourself_ch8.py
+++ try_it_yourself_ch8.py
@@ -35,3 +35,51 @@ def describe_city(city, country = "Ireland"):
 describe_city("reykjavik", "iceland")
 describe_city("dublin")
 describe_city("kilarney")
+
+
+print("\n--------------------------------------------------\n")
+# 8-6 City Names
+def city_country(city, country):
+	return(city.title() + ', ' + country.title() )
+
+print(city_country('santiago', 'chile') )
+print(city_country('belfast', 'ireland') )
+print(city_country('haining', 'china') )
+
+print("\n--------------------------------------------------\n")
+# 8-7 and 8-8 Album && User Albums
+
+def make_album(artist_name, album_title, tracks = ''):
+	album = {'name' : artist_name, 'title' : album_title}
+
+	if tracks:
+		album['tracks'] = tracks
+
+	return album
+
+print(make_album('vampire weekend', 'contra', 15))
+print(make_album('pink floyd', 'animals'))
+print(make_album('tycho', 'dive'))
+
+
+# while loop practice with make_album
+
+while True:
+	print("\nPlease tell me the artist's name and album, number of tracks is optional: ")
+	print("\n(ENTER Q AT ANY TIME TO QUIT)")
+	a_name = input("Artist name: ")
+	if a_name.upper() == 'Q':
+		break
+
+	al_name = input("Album name: ")
+	if al_name.upper() == 'Q':
+		break
+
+	track = input("Number of tracks: ")
+	if track.upper() == 'Q':
+		break
+
+	formatted_album = make_album(a_name, al_name, track)
+	
+	print("Following are the albums info: " + str(formatted_album) + ".")
+	
