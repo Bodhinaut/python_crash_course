3c37d7d89654937e047798efc07415ef5763b0c0
Kyle Shive <shivekyle@gmail.com>
2018-05-30 12:33:49 -0500

    initial

---
+++ pract8_functions.py3
@@ -0,0 +1,8 @@
+# practice basic functions
+
+def greet_user(username):
+	"""Display a simple greeting"""
+	print("Hello " + username.title() + ", how are you?")
+
+
+greet_user("kyle")
---
+++ try_it_yourself_ch8.py
@@ -0,0 +1,13 @@
+# 8 - 1 Message
+
+def display_message():
+	print("This chapter I am learning about methods err.. (cough cough.. I mean...) function")
+
+display_message()
+
+# 8 - 2 
+
+def favorite_book(title):
+	print("\nOne of my favorite books is " + title.title() + ".")
+
+favorite_book("Zen Baggage")
