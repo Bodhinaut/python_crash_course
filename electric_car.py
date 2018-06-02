dd6bd89f4d795f4fc5f80f6e8ffa7164f1280448
Kyle Shive <shivekyle@gmail.com>
2018-06-01 21:22:54 -0500

    classes and inheritance, parent - child

---
+++ electric_car.py
@@ -0,0 +1,46 @@
+class Car():
+	"""Simple attempt to represent a car."""
+
+	def __init__(self, make, model, year):
+		"""Initialize attributes to describe a car. """
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer_reading = 0
+
+	def update_odometer(self, mileage):
+		"""
+		Set the odometer reading to the given value. 
+		Reject the change if it attempts to roll the odometer back.
+		"""
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("You can't roll back the odometer!")
+
+	def increment_odometer(self, miles):
+		"""Add given amount to the odometer reading."""
+		if miles < 0:
+			print("You cannot add negative miles!")
+		else:
+			self.odometer_reading += miles
+			print("You have updated your odometer to: " + str(self.odometer_reading) + ".")
+		
+
+	def get_descriptive_name(self):
+		"""Return a neatly formatted descriptive name."""
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+		return long_name.title()
+
+	def read_odometer(self):
+		"""Print a statment showing the car's milage."""
+		print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+class ElectricCar(Car):
+	"""Represents aspects of a car, specific to electric vehicles."""
+	def __init__(self, make, model, year):
+		"""Initizlize attributes of the parent class."""
+		super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name() )
